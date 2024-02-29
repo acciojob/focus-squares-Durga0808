0b62bc80cecf0961f61f984498710f616886d451
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2024-02-29 11:37:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-document.addEventListner(DOMContentLoaded,function(){
+document.addEventListner("DOMContentLoaded",function(){
 	const squres=document.getElementsByClassName("square");
 
 	squres.forEach(squre=>{
