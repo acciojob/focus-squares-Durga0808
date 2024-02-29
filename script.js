c8b2aaf0439a21afe01b57090d70117cb582b91e
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2024-02-29 11:33:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+document.addEventListner(DOMContentLoaded,function(){
+	const squres=document.getElementsByClassName("square");
+
+	squres.forEach(squre=>{
+		squre.addEventListner("mouseover",function () {
+			squres.forEach(currsqre=>{
+				if(currsqre!=squre){
+					currsqre.style.backgroundColor = "#6F4E37";
+				}
+			});
+		});
+
+		squre.addEventListner("mouseout",function () {
+			squres.forEach(currsqre=>{
+				currsqre.style.backgroundColor = "#E6E6FA";
+			});
+		});
+	});
+	
+});
